16fff603910a265c4a6ccbe7b8c68208edddf566
sunnyandsnowy <15925210670@139.com>
2020-02-25 22:53:25 +0800

    2020.2.25

---
+++ practice_five.py
@@ -0,0 +1,121 @@
+#car = 'subaru'
+#print("Is car == 'subaru'? I predict True.")
+#print(car == 'subaru')
+
+#print("\nIs car == 'audi'? I predict False.")
+#print(car == 'audi')
+#age = 18
+#print(age<19)
+#print(age<=18)
+#print(age>=20)
+#print(age == 16)
+#print(age!=18)
+#print(age>15)
+#name = 'jake'
+#print(name == 'Jake')
+#print(name.upper() == "JAKE")
+#print(name.title() == 'Jake')
+#print(name == 'jake')
+
+#string_1 = "i am a person"
+#string_2 = "I am a person"
+#string_3 = "iamaperson"
+#print(string_1 == string_2)
+#print(string_1 == string_3)
+#print(string_1 == string_2.lower())
+
+#number_1 = 12
+#number_2 = 34
+#print(number_1 == number_2)
+#print(number_1 != number_2)
+#print(number_1 > number_2)
+#print(number_1 < number_2)
+#print(number_1 >= number_2)
+#print(number_1 <= number_2)
+#print(number_1 > 12 and number_2 >=20)
+#print(number_1 < 7 or number_2 >=30)
+
+#colours = ['red', 'brown', 'yellow', 'blue']
+#print('blue' in colours)
+#print('purple' in colours)
+#print("grey" not in colours)
+
+#alineColor = 'green'
+#if alineColor == 'green':
+	#print("Congratulations! You get five stars")
+
+#alineColor = 'blue'
+#if alineColor == 'green':
+	#print("Congratulations! You get five stars")
+
+#alineColor = 'green'
+#if alineColor == 'blue':
+	#print("Congratulations! You get five stars")
+#else :
+	#print("Congratulations! You get ten stars")
+	
+#alineColor = 'green'
+#if alineColor == 'yellow':
+	#print("Congratulations! You get five stars")
+#elif alineColor == 'yellow':
+	#print("Congratulations! You get ten stars")
+#else :
+	#print("Congratulations! You get fifteen stars")
+
+#age = 65
+#if age < 2:
+	#print("He is a baby")
+#elif age >= 2 and age < 4:
+	#print("He is learning walking slowly")
+#elif age >= 4 and age <13:
+	#print("He is a child")
+#elif age >= 13 and age < 20:
+	#print("He is a teenage")
+#elif age >= 20 and age < 65:
+	#print("He is an adult")
+#else:
+	#print("He is an old man")
+
+#favoriteFruits = ['strawberry', 'apple', 'pineapple']
+#if 'apple' in favoriteFruits:
+	#print("You really like apple")
+#if 'bananas' in favoriteFruits:
+	#print("You really like bananas")
+#if 'strawberry' in favoriteFruits:
+	#print("You really like strawberry")
+#if 'orange' in favoriteFruits:
+	#print("You really like orange")
+#if 'pear' in favoriteFruits:
+	#print("You really like pear")
+
+#users = ['jake', 'mike', 'ann', 'john', 'admin']
+#if users:
+#	for user in users:
+#		if user == 'admin':
+#			print('Hello admin, would you like to see a status report')
+#		else:
+#			print("Hello Eric, thank you for logging in again")
+#else:
+#	print("We need to find some users")
+
+
+#currentUsers = ['jake', 'mike', 'ann', 'john', 'admin']
+#newUsers = ['zzm', 'hyc', 'jake', 'mike', 'xzx']
+#for user in newUsers:
+#	if user.lower() in currentUsers:
+#		print("The name has already existed")
+#	else:
+#		print("You can use it")	
+
+
+#numbers = list(range(1,10))
+#for number in numbers:
+#	if number == 1:
+#		print("1st")
+#	elif number == 2:
+#		print("2nd")
+#	elif number == 3:
+#		print("3rd")
+#	else:
+#		print(str(number)+"th")	
+
